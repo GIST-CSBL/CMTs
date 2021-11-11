77918c67dfcbdfdfdf73fb481bfc610612097568
csbl-account <csbl.gist@gmail.com>
2021-11-11 10:57:36 +0900

    Update CMTs_visualization.R

--- CMTs_visualization.R
+++ CMTs_visualization.R
@@ -4,6 +4,7 @@
 #Import packages
 require(ggplot2)
 require(ggfortify)
+require(factoextra)
 
 #Import data
 #dataM: Preprocessed metabolite dataset. All values in this file were imputed using proper method.
@@ -13,16 +14,36 @@ dataM$group<-gsub("1", "Benign", dataM$group)
 dataM$group<-gsub("2", "Malignant", dataM$group)
 dataM$group<-gsub("3", "Normal", dataM$group)
 
+#If you need to exclude the outlier, please run the below code
+#x,y: the row number of outlier samples
+#dataM<-dataM[-c(x,y),]
+
 #Log2 transformation
 #For the PCA, data was transformed using log2 
 #Please ignore if you want to visualize the raw data
-dataM[,3:57]<-log(dataM[,3:57]+1, base=2)
+log_dataM<-dataM
+log_dataM[,3:57]<-log(log_dataM[,3:57]+1, base=2)
 
-#PCA
-pca_dataM<-prcomp(dataM[,-c(1,2)], scale.=TRUE)
-autoplot(pca_dataM)
-autoplot(pca_dataM, data=dataM, colour='breeds', frame.type='norm', frame.colour="group", shape='group', frame.alpha=0.05)+
-  theme_bw()+
-  theme(legend.key = element_rect(fill = "white"))+
-  scale_shape_manual(values=c(17, 16, 8))
+#PCA for the raw data
+pca_dataM<-prcomp(dataM[,-c(1,2)], scale.=TRUE) #remove groups and breeds features
+fviz_pca_ind(pca_dataM,
+             col.ind=dataM$group,
+             addEllipses = FALSE,
+             repel=TRUE)
 
+#PCA for the log2 transformed data
+pca_log_dataM<-prcomp(log_cmts[,-c(1,2)], scale.=TRUE) #remove groups and breeds features
+fviz_pca_ind(pca_log_dataM,
+             col.ind=log_dataM$breeds,
+             addEllipses = FALSE,
+             repel=TRUE,
+             labelsize=3)
+
+#Export the plot for TIFF
+tiff("PLS-DA.tiff", units="mm", width=190, height=180, res=300)
+fviz_pca_ind(pca_log_dataM,
+             col.ind=log_dataM$breeds,
+             addEllipses = FALSE,
+             repel=TRUE,
+             labelsize=3)
+dev.off()
