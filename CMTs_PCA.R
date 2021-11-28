65276ef5bc8ad20613840d04a330786556f3857f
csbl-account <csbl.gist@gmail.com>
2021-11-28 18:16:42 +0900

    Update and rename CMTs_statistical_analysis.R to CMTs_PCA.R

--- CMTs_PCA.R
+++ CMTs_PCA.R
@@ -15,13 +15,13 @@ dataM$group<-gsub("1", "Benign", dataM$group)
 dataM$group<-gsub("2", "Malignant", dataM$group)
 dataM$group<-gsub("3", "Normal", dataM$group)
 
-#### PCA plot ####
 #Log2 transformation
 #For the PCA, data was transformed using log2 
 #Please ignore if you want to visualize the raw data
 log_dataM<-dataM
 log_dataM[,3:57]<-log(log_dataM[,3:57]+1, base=2)
 
+#### PCA plot ####
 #PCA for the raw data
 pca_dataM<-prcomp(dataM[,-c(1,2)], scale.=TRUE) #remove groups and breeds features
 fviz_pca_ind(pca_dataM,
@@ -44,26 +44,4 @@ fviz_pca_ind(pca_log_dataM,
              addEllipses = FALSE,
              repel=TRUE,
              labelsize=3)
-dev.off()
-
-#### Dendrogram ####
-hc<-hclust(dist(log_dataM[,-c(1,2)]), method="average")
-dend<-as.dendrogram(hc)
-
-labels_colors(dend)<-
-  rainbow_hcl(3)[
-    as.numeric(log_cmts[,1])[order.dendrogram(dend)]]
-
-dend<-set(dend, "labels_cex", 0.7)
-plot(dend,
-     main="Clustered CMTs data set",
-     horiz=FALSE, nodePar=list(cex=.05))
-legend("topright", inset=.07, legend=c("Benign", "Malignant", "Normal"), fill=rainbow_hcl(3))
-
-tiff("Hierarchical_300_groups.tiff", units="mm", width=380, height=180, res=300) #1-column: 90mm, 2-column:190mm
-plot(dend,
-     main="Clustered CMTs data set",
-     horiz=FALSE, nodePar=list(cex=.05))
-legend("topright", inset=.07, legend=c("Benign", "Malignant", "Normal"), fill=rainbow_hcl(3))
-
 dev.off()
